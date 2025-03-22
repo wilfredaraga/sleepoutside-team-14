d9c38b548f46ec6716b58648d0b60fe6082be273
mefix007 <111250333+mefix007@users.noreply.github.com>
2025-03-22 16:01:59 +0100

    code modified

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 // <<<<<<< HEAD
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+// import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 // =======
 import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 
@@ -11,19 +11,19 @@ const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
 // <<<<<<< HEAD
-function addProductToCart(product) {
+// function addProductToCart(product) {
 // =======
-const product = new ProductDetails(productId, dataSource);
+// const product = new ProductDetails(productId, dataSource);
 
-product.init();
+// product.init();
 
-}
-function addProductToCart(products) {
+// }
+// function addProductToCart(products) {
 // >>>>>>> main
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+  // const cartItems = getLocalStorage("so-cart") || [];
+  // cartItems.push(product);
+  // setLocalStorage("so-cart", cartItems);
+// }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
