1d1ddc7cf78b25aa826286fa4e1e40a5b662f61a
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-03-25 15:24:00 +0000

    Correcting errors, adding strech activities by refactoring the cart.js code and creating the shoppingCart.mjs file. Also implement the callback function to display numbers of items in the loadHeaderFooter

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,43 +1,60 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+loadHeaderFooter();
 
-  totalInCart(cartItems);
-}
+import ShoppingCart from "./shoppingCart.mjs";
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
 
-  return newItem;
-}
+const cartItems = getLocalStorage("so-cart");
+console.log(cartItems);
 
-function totalInCart(itemsInCart) {
-  let sum = 0;
-  const cartTotal = document.querySelector(".cart-footer");
-  const totalPrice = document.querySelector(".cart-total");
-  for (let i = 0; i < itemsInCart.length; i++) {
-    sum += itemsInCart[i].FinalPrice;
-  }
-  if (itemsInCart.length > 0) {
-    cartTotal.style.display = "block";
-    totalPrice.innerHTML = `Total: ${sum.toFixed(2)}`;
-  }
-}
+const shoppingCart = new ShoppingCart(
+  cartItems,
+  "#cart-item-template",
+  ".product-list"
+);
 
-renderCartContents();
+shoppingCart.renderCartItems();
+
+
+// function renderCartContents() {
+//   const cartItems = getLocalStorage("so-cart");
+//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+//   totalInCart(cartItems);
+// }
+
+// function cartItemTemplate(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+// </li>`;
+
+//   return newItem;
+// }
+
+// function totalInCart(itemsInCart) {
+//   let sum = 0;
+//   const cartTotal = document.querySelector(".cart-footer");
+//   const totalPrice = document.querySelector(".cart-total");
+//   for (let i = 0; i < itemsInCart.length; i++) {
+//     sum += itemsInCart[i].FinalPrice;
+//   }
+//   if (itemsInCart.length > 0) {
+//     cartTotal.style.display = "block";
+//     totalPrice.innerHTML = `Total: ${sum.toFixed(2)}`;
+//   }
+// }
+
+// renderCartContents();
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
 const dataSource = new ProductData("tents");
 
 
@@ -10,23 +11,6 @@ const listElement = document.querySelector(".product-list") || document.createEl
 const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
 
-function updateCartCount() {
-  // Get cart items from localStorage (or initialize an empty array if none exist)
-  const cartItems = getLocalStorage("so-cart") || [];
 
-  // Calculate the total number of items in the cart
-  let totalItems = 0;
-  cartItems.forEach(item => {
-    totalItems += item.quantity || 1; // Add item quantity, default to 1 if not defined
-  });
 
-  // Select the <em> element
-  const cartCountElement = document.querySelector(".cart em");
 
-  // Update the <em> element's content with the total number of items
-  if (cartCountElement) {
-    cartCountElement.textContent = totalItems;
-  }
-}
-
-updateCartCount();
