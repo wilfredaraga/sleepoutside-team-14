85bad8dffb8ba04125fa793346e6015958c788b8
wilfredaraga <wilfredeiron@gmail.com>
2025-03-26 16:23:10 +0800

    fixing little errors after merging to main branch

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -35,41 +35,6 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
     parentElement.insertAdjacentHTML(position, list.map(templateFn).join(""));
 }
 
-export function renderWithTemplate(template, parentElement, data, callback){
-    parentElement.innerHTML = template;
-    if(callback) {
-      callback(data);
-    }
-}
-
-
-
-export async function loadTemplate(path){
-    const res = await fetch(path);
-  const template = await res.text();
-  return template;
-}
-
-export async function loadHeaderFooter(){
-    
-   const headerTemplate = await loadTemplate("../partials/header.html");
-   
-  const footerTemplate = await loadTemplate("../partials/footer.html");
-
-  const headerElement = document.querySelector("#main-header");
- 
-  const footerElement = document.querySelector("#main-footer");
-
-  const cartItems = getLocalStorage("so-cart") || [];
-  
-  renderWithTemplate(headerTemplate, headerElement,cartItems,updateCartCount);
-  renderWithTemplate(footerTemplate, footerElement);
-
-    
-    
-}
-
-
 export function updateCartCount(cartItems) {
   // Get cart items from localStorage (or initialize an empty array if none exist)
  
