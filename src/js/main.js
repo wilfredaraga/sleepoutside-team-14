3924dfe668d9b89d6de9558e7a14da1d66065162
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-04-09 11:14:04 +0000

    Correcting Main.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import Alert from "./AlertMessage.mjs";
 
 import {  loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-const dataSource = new ProductData("tents");
+const Services = new ExternalServices();
+const dataSource = Services.getData("tents");
 
 const info = new Alert("../json/alert.json", "main");
 
 
-// info.displayAlerts();
+ info.displayAlerts();
 const listElement = document.querySelector(".product-list") || document.createElement("ul");
 const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
