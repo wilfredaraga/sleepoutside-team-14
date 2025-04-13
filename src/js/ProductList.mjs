679395409c104b3edda0d0d6fe02ecfab6efdcfa
mefix007 <111250333+mefix007@users.noreply.github.com>
2025-04-13 15:08:37 +0100

    Update ProductList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,16 +1,54 @@
+import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-  return `<li class="product-card">
-    <a href="/product_pages/?product=${product.Id}">
-      <img src="${product.Images.PrimaryMedium}" alt="${product.NameWithoutBrand} ">
-      <h2 class="card__brand">${product.Brand.Name}11</h2>
-      <h3 class="card__name">${product.NameWithoutBrand}000</h3>
-      <p class="product-card__price">$${product.ListPrice}</p>
-    </a>
-  </li>`
+    return `<li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+            <img
+            src="${product.Image}"
+            alt="${product.NameWithoutBrand}"/>
+            <h2>${product.Brand.Name}</h2>
+            <h3>${product.NameWithoutBrand}</h3>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+            </a>
+        </li>`;
 }
 
-import { renderListWithTemplate } from "./utils.mjs";
+// export default class ProductList{
+//     constructor(category, dataSource, listElement){
+//         // You passed in this information to make the class as reusable as possible.
+//         // Being able to define these things when you use the class will make it very flexible
+//         this.category = category;
+//         this.dataSource = dataSource;
+//         this.listElement = listElement;
+//     }
+
+//     async init() {
+//        try {
+//         // the dataSource will return a Promise...so you can use await to resolve it.
+//         const list = await this.dataSource.getData();
+//         // next, render the list – ** future **
+//         this.renderList(list);
+//        } catch (error) {
+//         console.log(error)
+//        }
+//     }
+//        renderList(list) {
+//         renderListWithTemplate(productCardTemplate, this.listElement, list)
+//         };
+// };
+// =======
+// function productCardTemplate(product) {
+//   return `<li class="product-card">
+//     <a href="product_pages/?product=${product.Id}">
+//       <img src="${product.Image}" alt="${product.NameWithoutBrand} ">
+//       <h2 class="card__brand">${product.Brand.Name}11</h2>
+//       <h3 class="card__name">${product.NameWithoutBrand}000</h3>
+//       <p class="product-card__price">$${product.ListPrice}</p>
+//     </a>
+//   </li>`
+// }
+
+// import { renderListWithTemplate } from "./utils.mjs";
 
 export default class ProductList {
 
@@ -25,7 +63,7 @@ export default class ProductList {
          
     try {
         
-       const list = await this.dataSource.getData(this.category);
+       const list = await this.dataSource.getData();
          this.renderList(list);
     } catch (error) {
        //console.error(error);
@@ -38,10 +76,5 @@ export default class ProductList {
         renderListWithTemplate(productCardTemplate,this.listElement,list);
        
     }
-
-    
-
-
-  
 
 }
